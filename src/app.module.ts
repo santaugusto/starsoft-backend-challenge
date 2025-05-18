757484d8f67ce08718b6c346c141981becc3dc82
santaugusto <sanntaugusto@gmail.com>
2025-05-18 19:58:14 -0300

    ♻️ App setup
    Atualiza AppModule e bootstrap com novos módulos de pedidos, itens, Kafka e Elasticsearch.
    
    Reflete a integração entre os módulos e inicialização da aplicação.
    
    Reviewed-by: Nome Do Revisor Refs #105

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { PedidosModule } from './pedidos/pedidos.module';
+import { ItensModule } from './itens/itens.module';
+import { KafkaModule } from './kafka/kafka.module';
+import { MyElasticsearchModule } from './elasticsearch/elasticsearch.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Carregar variáveis de ambiente globalmente
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Conexão com PostgreSQL via TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('POSTGRES_HOST'),
+        port: Number(config.get<number>('POSTGRES_PORT')),
+        username: config.get('POSTGRES_USER'),
+        password: config.get('POSTGRES_PASSWORD'),
+        database: config.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true, // desative em produção
+      }),
+    }),
+
+    MyElasticsearchModule,
+    KafkaModule,
+    PedidosModule,
+    ItensModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  app.enableCors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Pedidos')
+    .setDescription('Documentação da API do sistema de e-commerce')
+    .setVersion('1.0')
+    .addServer('/')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+ 
+  await app.listen(process.env.PORT ?? 3002);
+
+
 }
 bootstrap();
