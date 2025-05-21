8144f5fca8e1f7d5f99e6efc5254cd757c468275
santaugusto <sanntaugusto@gmail.com>
2025-05-20 21:10:51 -0300

    refactor: atualiza app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,13 @@ import { ItensModule } from './itens/itens.module';
 import { KafkaModule } from './kafka/kafka.module';
 import { MyElasticsearchModule } from './elasticsearch/elasticsearch.module';
 
+import { WinstonLoggerService } from './common/logger/winston-logger.service';
+import { PrometheusModule } from './common/prometheus/prometheus.module';
+import { PrometheusInterceptor } from './common/interceptors/prometheus.interceptor';
+import { LoggerModule } from './common/logger/logger.module';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -27,11 +34,23 @@ import { MyElasticsearchModule } from './elasticsearch/elasticsearch.module';
         synchronize: true,
       }),
     }),
-
+    PrometheusModule,
     MyElasticsearchModule,
     KafkaModule,
     PedidosModule,
     ItensModule,
+    LoggerModule,
   ],
+  providers: [
+    WinstonLoggerService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: PrometheusInterceptor,
+    },],
+  exports: [WinstonLoggerService],
 })
-export class AppModule {}
+export class AppModule { }
